3ee5b753ff4c62d816dba09e1e88cac01284b878
giyana <wktkktkr5@gmail.com>
2021-07-15 20:20:28 +0900

    二週目

--- html/cms/routes/web.php
+++ html/cms/routes/web.php
@@ -14,21 +14,29 @@
 use App\Book;
 use Illuminate\Http\Request;
 
-//本ダッシュボード表示
-Route::get('/', 'BooksController@index');
+/**
+* 本の一覧表示(books.blade.php)
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//登録処理
-Route::post('/books', 'BooksController@store');
+/**
+* 本を追加 
+*/
+Route::post('/books', function (Request $request) {
+    //
+});
 
-//更新画面
-Route::post('/booksedit/{books}', 'BooksController@edit');
+/**
+* 本を削除 
+*/
+Route::delete('/book/{book}', function (Book $book) {
+    //
+});
 
-//更新処理
-Route::post('/books/update', 'BooksController@update');
 
-//本を削除
-Route::delete('/book/{book}', 'BooksController@destroy');
-
-//Auth
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
+
